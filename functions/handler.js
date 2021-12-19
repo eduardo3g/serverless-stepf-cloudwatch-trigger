420d52d3abeb475492532e16984e3d0cca9b9b4f
eduardo3g <eduardosbrasil10@gmail.com>
2021-12-19 16:03:16 -0300

    feat: trigger state machine from cloudwatch and s3 bucket PutObject operation

--- functions/handler.js
+++ functions/handler.js
@@ -1,11 +1,3 @@
 module.exports.handler = async (event) => {
   console.log(JSON.stringify(event));
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from Step Functions!',
-      input: event,
-    }),
-  };
 };
